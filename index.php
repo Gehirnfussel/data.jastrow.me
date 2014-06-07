555b839ecce3dc324f271c49eb6476a73141a314
Jan Jastrow <jan@schwerkraftlabor.de>
2014-06-07 14:21:19 +0200

    Some lonely data

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+#¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+#   name.........: data.jastrow.me
+#   version......: 0.1
+#   created.by...: Jan Jastrow
+#   contact......: jan@schwerkraftlabor.de
+#   license......: MIT license
+#   source.......: https://github.com/Gehirnfussel/data.jastrow.me
+#_________________
+
+
+#|:· Debugmode
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="dns-prefetch" href="//fonts.googleapis.com">
+	<link rel="dns-prefetch" href="//schwerkraftlabor.de">
+	<link rel="shortcut icon" type="image/png" href="favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
+	<title>data.jastrow.me &middot; raw data about Jan</title>
+	<link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+	<link href="css/main.css" rel="stylesheet" />
+	<meta name="description" content="Some basic and raw data about Jan Jastrow." />
+	<meta name="author" content="Jan Jastrow" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<link type="text/plain" rel="author" href="humans.txt" />
+</head>
+<body>
+
+<table><tbody>
+<tr>
+	<td>fullname</td>
+	<td>Jan Jastrow</td>
+</tr>
+<tr>
+	<td>sex</td>
+	<td>male</td>
+</tr>
+<tr>
+	<td>twitter</td>
+	<td>@Gehirnfussel</td>
+</tr>
+<tr>
+	<td>drinks_beer</td>
+	<td>no</td>
+</tr>
+
+</tbody></table>
+
+<footer>
+</footer>
+</body>
+</html>
